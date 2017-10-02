3cde51232c7d693050680469658c981b8ad95476
Maddie Phillips <mphillips@c5ci.com>
2017-10-02 09:35:14 -0400

    Add hero image (needs styling still)

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,17 @@ import Nav from './Nav'
 const Header = () => (
   <div>
     <Nav />
+    <img class="hero-image" src="/static/hero-image.jpg" />
+    <style jsx>{`
+      .hero-image {
+        width: 100%;
+      }
+      `}</style>
+      <style global jsx>{`
+      body {
+        background-color: rgb(242,242,242);
+      }
+      `}</style>
   </div>
 )
 
