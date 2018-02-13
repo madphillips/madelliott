4bb6fbb186aad2b4b5edb44b0d32cedbd5a6d9a9
Maddie Phillips <mphillips@c5ci.com>
2018-02-13 00:28:21 -0500

    Add an FAQ section

--- components/Details.js
+++ components/Details.js
@@ -16,15 +16,17 @@ const DetailsWrapper = styled.div`
   }
 `;
 
+const FlexContainer = styled(Flex)`
+  margin: 2em 4em;
+`;
 
 const Col = styled(Box)`
-  min-width: 400px;
 `;
 
 const Details = () => (
   <DetailsWrapper id="wedding-details">
     <h2>DETAILS</h2>
-    <Flex wrap="wrap" justify="center">
+    <FlexContainer wrap="wrap" justify="center">
       <Col w={[ 1, 1/2]}>
         <h3>Accomodations</h3>
         <p>The nearest hotels are in the Short Pump area, which is about a 20 minute drive from The Mill at Fine Creek- below are a couple of choices.</p>
@@ -54,38 +56,13 @@ Veil Brewery has the best IPAs in town, Buskey Cidery satisfies a cider fix, Taz
         </p>
         <h4>Going into Charlottesville?</h4>
         <p>
-        Our favorite place in Charlottesville is the Historic Downtown Mall, which is an outdoor mall and completely dog friendly. We love bringing our pup and sitting in Citizen Burger’s outdoor patio. The Sprint Pavilion is on the far east side of the Downtown Mall and we have caught a couple of concerts there! For the historian in you, Monticello is also a quick drive south of Charlottesville with a colonial dining experience at Michie Tavern right around the corner! Looking for a drink? Charlottesville’s beer scene is starting to grown and is also surrounded by numerous excellent wineries, we have yet to try one we didn’t like!
+        Our favorite place in Charlottesville is the Historic Downtown Mall, which is an outdoor mall and completely dog friendly. We love bringing our pup and sitting in Citizen Burger’s outdoor patio. The Sprint Pavilion is on the far east side of the Downtown Mall and we have caught a couple of concerts there!
+        </p>
+        <p>
+          For the historian in you, Monticello is also a quick drive south of Charlottesville with a colonial dining experience at Michie Tavern right around the corner! Looking for a drink? Charlottesville’s beer scene is starting to grown and is also surrounded by numerous excellent wineries, we have yet to try one we didn’t like!
         </p>
       </Col>
-      <Col w={ 1 }>
-        <h3>The Wedding</h3>
-        <ul>
-          <li>
-            <h4>When and where is it?</h4>
-            <p>Friday, March 23, 2018 from 6pm until 11pm<br />
-              The Mill at Fine Creek<br />
-              2434 Robert E Lee Rd, Powhatan, VA 23139
-            </p>
-            <p>The Mill at Fine Creek is approximately a 30 minute drive from Richmond and an hour drive from Charlottesville.</p>
-          </li>
-          <li>
-            <h4>What should I wear?</h4>
-            <p>The dress code for the night is semi-formal. It is recommended that you plan for cold and/or inclement weather, as the ceremony will take place outdoors.
-            </p>
-          </li>
-          <li>
-            <h4>Do you have a timeline?</h4>
-            <p>As of right now, we have not finalized our timeline. Seating for the ceremony will begin at 5:30pm and the reception will conclude at 11pm.
-            </p>
-          </li>
-          <li>
-            <h4>Will you have transportation? If not, where do I park?</h4>
-            <p>We will not have transportation to or from the venue. The Mill at Fine Creek has a very large parking lot on site right in front of the main entrance. Please drink responsibly and arrange to have a designated driver to be safe!
-            </p>
-          </li>
-        </ul>
-      </Col>
-    </Flex>
+    </FlexContainer>
   </DetailsWrapper>
 )
 
