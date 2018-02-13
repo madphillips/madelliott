704004e2c76658b45cdfbb9c1710487b0eb4404d
Elliott Pogue <epogue@gmail.com>
2018-02-13 00:20:25 -0500

    Animate Quotes

--- components/About.js
+++ components/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Flex, Box} from 'grid-styled';
 import Anchor from "./Anchor";
+import TrackVisibility from "react-on-screen";
 import styled, { injectGlobal } from "styled-components";
 import { COLORS, FONTS, HEADERSIZE } from '../constants';
 import { P } from "./Text";
+import { shade } from "polished";
 import Container from "./Container";
 
 const AboutWrapper = styled.div`
@@ -34,10 +36,10 @@ const ImageWrapper = styled.div`
 const Quote = styled.p`
   font-size: 1em;
   line-height: 1.25em!important;
-  border: 1px solid ${COLORS.darkgray};
+  box-shadow: 0 1px 8px rgba(0, 0, 0, 0.1);
   padding: 1.5em 2em;
-  background-color: ${COLORS.gray};
-  color: ${props => COLORS[props.color || "darkgray"]};
+  background: #fff;
+  color: ${props => shade(0.8, COLORS[props.color || "darkgray"])};
   margin: 1.5em 0;
   width: 350px;
   position: absolute;
@@ -45,6 +47,12 @@ const Quote = styled.p`
   right: ${props => [props.right || "auto"]};
   top: ${props => [props.top || "auto"]};
   bottom: ${props => [props.bottom || "auto"]};
+  opacity: ${props => props.isVisible ? 1 : 0};
+  transform: translateY(${props => props.isVisible ? "-20%" : 0});
+  transition-property: opacity, transform;
+  transition-duration: 0.45s;
+  transition-timing-function: ease-out;
+  transition-delay: ${props => props.delay || 0}ms;
 `;
 
 const About = props => (
@@ -63,17 +71,21 @@ const About = props => (
         </P>
       </Col>
       <Col width="55%">
-        <ImageWrapper>
-          <img src="/static/engagement-photo-1.jpg" width="435" height="649"/>
-          <Quote color="blue" left="-5%" top="-10px">
-            "Elliott is one of the constants in my life. Always so kind and full of laughter. He is forever supportive
-            and I'm so thankful that he pushes me to be the best I can be. I can't imagine life without him."
-          </Quote>
-          <Quote color="pink" right="-5%" bottom="0">
-            "Ever since I first saw Maddie's smile, I knew she was the one for me. I look forward to every day I get to
-            spend with her and her engaging and exuberant personality."
-          </Quote>
-        </ImageWrapper>
+        <TrackVisibility offset={200}>
+          {({ isVisible }) => (
+            <ImageWrapper>
+              <img src="/static/engagement-photo-1.jpg" width="435" height="649"/>
+              <Quote color="pink" left="-40px" top="-10px" isVisible={isVisible}>
+                "Elliott is one of the constants in my life. Always so kind and full of laughter. He is forever supportive
+                and I'm so thankful that he pushes me to be the best I can be. I can't imagine life without him."
+              </Quote>
+              <Quote color="blue" right="-40px" bottom="0" delay={200} isVisible={isVisible}>
+                "Ever since I first saw Maddie's smile, I knew she was the one for me. I look forward to every day I get to
+                spend with her and her engaging and exuberant personality."
+              </Quote>
+            </ImageWrapper>
+          )}
+        </TrackVisibility>
       </Col>
     </Container>
   </AboutWrapper>
