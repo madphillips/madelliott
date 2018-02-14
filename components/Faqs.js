24d8cca909c56de0ce0da4de94f4232804404708
Elliott Pogue <epogue@gmail.com>
2018-02-14 12:07:51 -0500

    Set up click handlers

--- components/Faqs.js
+++ components/Faqs.js
@@ -42,54 +42,75 @@ const FaqSectionTitle = styled.div`
   }
 `;
 
-const Faqs = () => (
-  <FaqsWrapper id="faqs">
-    <h2>Have Questions? Hopefully We Have Your Answer!</h2>
-    <FlexContainer wrap="wrap" justify="center">
-      <Col id="faq" w={[1/2, 1/3, 1/4 ]}>
-        <FaqSectionTitle>
-          <img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png" />
-          <span>Ceremony Questions</span>
-        </FaqSectionTitle>
-      </Col>
-      <Col id="faq" w={[1/2, 1/3, 1/4 ]}>
-        <FaqSectionTitle>
-          <img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png" />
-          <span>Guest Questions</span>
-        </FaqSectionTitle>
-      </Col>
-      <Col id="faq" w={[1/2, 1/3, 1/4 ]}>
-      <FaqSectionTitle>
-          <img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png" />
-          <span>Media Questions</span>
-        </FaqSectionTitle>
-      </Col>
-      <Col id="faq" w={[1/2, 1/3, 1/4 ]}>
-      <FaqSectionTitle>
-          <img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png" />
-          <span>Food &amp; Drink Questions</span>
-        </FaqSectionTitle>
-      </Col>
-      <Col id="faq" w={[1/2, 1/3, 1/4 ]}>
-      <FaqSectionTitle>
-          <img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png" />
-          <span>Transportation Questions</span>
-        </FaqSectionTitle>
-      </Col>
-      <Col id="faq" w={[1/2, 1/3, 1/4 ]}>
-      <FaqSectionTitle>
-          <img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png" />
-          <span>Registry Questions</span>
-        </FaqSectionTitle>
-      </Col>
-      <Col id="faq" w={[1/2, 1/3, 1/4 ]}>
-      <FaqSectionTitle>
-          <img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png" />
-          <span>Contact Us</span>
-        </FaqSectionTitle>
-      </Col>
-    </FlexContainer>
-  </FaqsWrapper>
-)
+class Faqs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSectionClick = this.handleSectionClick.bind(this);
+    this.state = {
+      section: null
+    }
+  }
+
+  handleSectionClick(e) {
+    const { target } = e;
+    if (target && target.dataSet && target.dataSet.section) {
+      this.setState({
+        section: target.dataSet.section
+      });
+    }
+  }
+
+  render() {
+    return (
+      <FaqsWrapper id="faqs">
+        <h2>Have Questions? Hopefully We Have Your Answer!</h2>
+        <FlexContainer wrap="wrap" justify="center">
+          <Col w={[1/2, 1/3, 1/4 ]}>
+            <FaqSectionTitle onClick={this.handleSectionClick} data-section="ceremony">
+              <img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png" />
+              <span>Ceremony Questions</span>
+            </FaqSectionTitle>
+          </Col>
+          <Col w={[1/2, 1/3, 1/4 ]}>
+            <FaqSectionTitle onClick={this.handleSectionClick} data-section="guest">
+              <img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png" />
+              <span>Guest Questions</span>
+            </FaqSectionTitle>
+          </Col>
+          <Col w={[1/2, 1/3, 1/4 ]}>
+            <FaqSectionTitle onClick={this.handleSectionClick} data-section="media">
+              <img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png" />
+              <span>Media Questions</span>
+            </FaqSectionTitle>
+          </Col>
+          <Col w={[1/2, 1/3, 1/4 ]}>
+          <FaqSectionTitle onClick={this.handleSectionClick} data-section="food">
+              <img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png" />
+              <span>Food &amp; Drink Questions</span>
+            </FaqSectionTitle>
+          </Col>
+          <Col w={[1/2, 1/3, 1/4 ]}>
+          <FaqSectionTitle onClick={this.handleSectionClick} data-section="transportation">
+              <img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png" />
+              <span>Transportation Questions</span>
+            </FaqSectionTitle>
+          </Col>
+          <Col w={[1/2, 1/3, 1/4 ]}>
+          <FaqSectionTitle onClick={this.handleSectionClick} data-section="registry">
+              <img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png" />
+              <span>Registry Questions</span>
+            </FaqSectionTitle>
+          </Col>
+          <Col w={[1/2, 1/3, 1/4 ]}>
+          <FaqSectionTitle onClick={this.handleSectionClick} data-section="contact">
+              <img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png" />
+              <span>Contact Us</span>
+            </FaqSectionTitle>
+          </Col>
+        </FlexContainer>
+      </FaqsWrapper>
+    )
+  }
+}
 
 export default Faqs
